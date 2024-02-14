85efbf847730c55d6baf5aaf5505d5c60976f92d
Artem <artem.alshan@yandex.ru>
2024-02-14 19:46:40 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def my_func(func):
+
+    def wrapper(*args, **kwargs):
+        print('start')
+        func(*args, **kwargs)
+        print('end')
+
+    return wrapper
+
+
+def some_func(title):
+    print(f'{title}')
+
+
+some_func = my_func(some_func)
+some_func(title='test')
